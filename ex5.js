bbd62ba0376bb7029356be55a2ab6ae4fe7de2c9
Augusto Pires <mpires.augusto@gmail.com>
2023-06-21 20:41:33 -0300

    Adicionando ex5.js já implementado ao index.html

---
+++ ex5.js
@@ -0,0 +1,11 @@
+let palavras = ['Acelga', 'Cenoura', 'Nabo', 'Cogumelo', 'Limão', 'Batata', 'Peixe'];
+console.log(palavras)
+
+for (let lista of palavras) {
+    
+    if (lista.length > 5) {
+    console.log(lista);
+}
+}
+
+alert('Abrir o DevTools(F12) em Console.')
